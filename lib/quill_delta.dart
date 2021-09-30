80b44723b304a7761405c47b6fe08b73fe8840b1
Steve <steve@ecoportal.co.nz>
2021-09-30 14:57:06 +1300

    fix the fucking code

--- lib/quill_delta.dart
+++ lib/quill_delta.dart
@@ -798,3 +798,25 @@ class DeltaIterator {
     return op;
   }
 }
+
+
+/// Generates a hash code for multiple [objects].
+int hashObjects(Iterable objects) => _finish(objects.fold(0, (h, i) => _combine(h, i.hashCode)));
+
+/// Generates a hash code for two objects.
+int hash2(a, b) => _finish(_combine(_combine(0, a.hashCode), b.hashCode));
+
+/// Generates a hash code for three objects.
+int hash3(a, b, c) => _finish(_combine(_combine(_combine(0, a.hashCode), b.hashCode), c.hashCode));
+
+int _combine(int hash, int value) {
+  hash = 0x1fffffff & (hash + value);
+  hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+  return hash ^ (hash >> 6);
+}
+
+int _finish(int hash) {
+  hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+  hash = hash ^ (hash >> 11);
+  return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+}
